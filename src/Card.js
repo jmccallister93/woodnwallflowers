0d501523ce64fe2f1652214f9e141c9b3cdbd307
jmccallister93 <jaredmccallister93@gmail.com>
2022-10-06 14:31:36 -0700

    cards styled

--- src/Card.js
+++ src/Card.js
@@ -1,26 +1,16 @@
 import "./Card.scss";
 
-import carousel_img01 from "../src/Images/bank_circle.jpg";
-import carousel_img02 from "../src/Images/birthday_40.jpg";
-import carousel_img03 from "../src/Images/champ_wall2.jpg";
-
 const Card = () => {
   return (
     <div className="card__wrapper">
-      <div className="card">
-        <img src={carousel_img01} alt="" className="card__image" />
+      <div className="card" id="card__gallery">
         <p className="card__text">Gallery</p>
-        <button className="card__button">More Info!</button>
       </div>
-      <div className="card">
-        <img src={carousel_img02} alt="" className="card__image" />
+      <div className="card" id="card__pricing">
         <p className="card__text">Pricing</p>
-        <button className="card__button">More Info!</button>
       </div>
-      <div className="card">
-        <img src={carousel_img03} alt="" className="card__image" />
+      <div className="card" id="card__contact">
         <p className="card__text">Contact</p>
-        <button className="card__button">More Info!</button>
       </div>
     </div>
   );
