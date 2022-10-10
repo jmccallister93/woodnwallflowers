3cf977f815df2aeeedb7b9185cc4e3185b7a17f6
jmccallister93 <jaredmccallister93@gmail.com>
2022-10-10 16:47:39 -0700

    mobile sorta?

--- src/Home.js
+++ src/Home.js
@@ -7,12 +7,6 @@ const Home = () => {
       <h1 className="home__header">Welcome to Wood n Wallflowers</h1>
       <span className="home__subheader">BACKDROPS, BALLOONS, & MORE!</span>
       <Card />
-      <div className="about">
-        <h1 className="home__header">About Us</h1>
-        <p>
-            Blah Blah Blah
-        </p>
-      </div>
     </div>
   );
 };
