d1253dd68f0fd118e4f65863d4ffe2116cb30936
jmccallister93 <jaredmccallister93@gmail.com>
2022-10-13 21:35:48 -0700

    Fuck the burger menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
   const navLogo = <img src={logo} alt="No img" className="logo" />
   const navLinks = (
     <div className="links__wrapper">
-      <Socials /> 
+      
       <ul className="links">
         <Link to="/woodnwallflowers" style={{ textDecoration: "none", color: '#3E7C17' }}>
           <li className="links-item">HOME</li>
@@ -24,6 +24,7 @@ const Navbar = () => {
           <li className="links-item">CONTACT</li>
         </Link>
       </ul>
+      <Socials /> 
     </div>
   );
 
@@ -32,13 +33,13 @@ const Navbar = () => {
     
       {navLogo}
       {navLinks}
-      <Menu width={ '100%' } isOpen={ false} customBurgerIcon={ <img src={burger} alt='here lies the thing' /> }>
+      {/* <Menu width={ '100%' } isOpen={ false} customBurgerIcon={ <img src={burger} alt='here lies the thing' /> }>
         <a id="home" className="menu-item" href="/woodnwallflowers">HOME</a>
         <a id="about" className="menu-item" href="/pricing">PRICING</a>
         <a id="gallery" className="menu-item" href="/gallery">GALLERY</a>
         <a id="contact" className="menu-item" href="/contact">CONTACT</a>
         <Socials /> 
-      </Menu>
+      </Menu> */}
     </div>
   );
 };
